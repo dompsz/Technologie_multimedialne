f6d0b965a8071d0c93d31626e194590c79b8ddcc
dompsz <dompszcz@gmail.com>
2026-04-21 10:11:09 +0200

    Update db_config.php

--- lab18/db_config.php
+++ lab18/db_config.php
@@ -12,7 +12,7 @@ $db_pass = $_ENV['DB_PASS'] ?? '';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
     die("Błąd połączenia Lab 18: " . $e->getMessage());
